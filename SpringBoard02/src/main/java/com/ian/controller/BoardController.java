c5d362f7c476e3eb66443b9bffb2bb5945d71f81
ian <loundo001@gmail.com>
2026-02-15 18:08:42 +0900

    07. Spring MVC01버전 만들기 (스프링 기초 다지기)

---
+++ SpringBoard02/src/main/java/com/ian/controller/BoardController.java
@@ -0,0 +1,9 @@
+package com.ian.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BoardController {
+	
+	
+}
--- SpringBoard02/src/main/java/com/ian/controller/HomeController.java
+++ SpringBoard02/src/main/java/com/ian/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.ian.springboard;
+package com.ian.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
